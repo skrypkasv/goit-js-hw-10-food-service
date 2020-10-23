a11719750cf0fe303fe501b60c26f12bef152a4a
skrypkasv <skrypkasv@gmail.com>
2020-10-23 16:47:20 +0300

    Updates final results

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { checkbox, checkForSavedTheme, checkboxChangeHandler } from './js/theme';
+checkbox.addEventListener('change', checkboxChangeHandler);
+document.addEventListener('DOMContentLoaded', checkForSavedTheme);
+
+import data from './js/menu.json';
+import menuItemsTmpl from './templates/menu-items.hbs';
+const ul = document.querySelector('ul.js-menu');
+
+ul.innerHTML = menuItemsTmpl(data);
